d4e129ba9e412cf76ce519d0b311d1a175d66508
Александр Пыркин <Tyuiu.PyrkinAA@outlook.com>
2026-01-10 18:35:16 +0500

    1

--- Tyuiu.PyrkinAA.Sprint5.Task2.V14.Lib/DataService.cs
+++ Tyuiu.PyrkinAA.Sprint5.Task2.V14.Lib/DataService.cs
@@ -8,21 +8,23 @@ namespace Tyuiu.PyrkinAA.Sprint5.Task2.V14.Lib
     {
         public string SaveToFileTextData(int[,] matrix)
         {
-         
+    
+            string tempPath = Path.GetTempPath();
             string fileName = "OutPutFileTask2.csv";
+            string path = Path.Combine(tempPath, fileName);
 
           
-            if (File.Exists(fileName))
+            if (File.Exists(path))
             {
-                File.Delete(fileName);
+                File.Delete(path);
             }
 
-          
+           
             int rows = matrix.GetLength(0);
             int cols = matrix.GetLength(1);
 
          
-            using (StreamWriter writer = new StreamWriter(fileName, false))
+            using (StreamWriter writer = new StreamWriter(path, false))
             {
                 for (int i = 0; i < rows; i++)
                 {
@@ -30,17 +32,16 @@ namespace Tyuiu.PyrkinAA.Sprint5.Task2.V14.Lib
 
                     for (int j = 0; j < cols; j++)
                     {
-                      
+                       
                         values[j] = matrix[i, j] > 0 ? "1" : "0";
                     }
 
-                  
+             
                     writer.WriteLine(string.Join(";", values));
                 }
             }
 
-           
-            return Path.GetFullPath(fileName);
+            return path;
         }
     }
 }
--- Tyuiu.PyrkinAA.Sprint5.Task2.V14.Test/DataServiceTest.cs
+++ Tyuiu.PyrkinAA.Sprint5.Task2.V14.Test/DataServiceTest.cs
@@ -10,6 +10,7 @@ namespace Tyuiu.PyrkinAA.Sprint5.Task2.V14.Test
         [TestMethod]
         public void ValidSaveToFileTextData()
         {
+         
             DataService ds = new DataService();
             int[,] matrix = {
                 {-3, -6, -3},
@@ -17,38 +18,23 @@ namespace Tyuiu.PyrkinAA.Sprint5.Task2.V14.Test
                 {-6, -5, 5}
             };
 
+      
             string path = ds.SaveToFileTextData(matrix);
 
-            Assert.IsTrue(File.Exists(path), "Файл не был создан!");
+      
+            Assert.IsTrue(File.Exists(path), $"Файл не найден: {path}");
 
-            string[] fileLines = File.ReadAllLines(path);
-            Assert.AreEqual(3, fileLines.Length, "Неверное количество строк в файле!");
-            Assert.AreEqual("0;0;0", fileLines[0], "Неверная первая строка!");
-            Assert.AreEqual("1;1;1", fileLines[1], "Неверная вторая строка!");
-            Assert.AreEqual("0;0;1", fileLines[2], "Неверная третья строка!");
+            string[] lines = File.ReadAllLines(path);
+            Assert.AreEqual(3, lines.Length);
+            Assert.AreEqual("0;0;0", lines[0]);
+            Assert.AreEqual("1;1;1", lines[1]);
+            Assert.AreEqual("0;0;1", lines[2]);
 
-            File.Delete(path);
-        }
-
-        [TestMethod]
-        public void CheckedArrayOfDifferentSize()
-        {
-            DataService ds = new DataService();
-            int[,] matrix = {
-                {1, -2, 3, -4},
-                {-5, 6, -7, 8}
-            };
-
-            string path = ds.SaveToFileTextData(matrix);
-
-            Assert.IsTrue(File.Exists(path), "Файл не был создан!");
-
-            string[] fileLines = File.ReadAllLines(path);
-            Assert.AreEqual(2, fileLines.Length, "Неверное количество строк!");
-            Assert.AreEqual("1;0;1;0", fileLines[0], "Неверная первая строка!");
-            Assert.AreEqual("0;1;0;1", fileLines[1], "Неверная вторая строка!");
-
-            File.Delete(path);
+            
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
         }
     }
 }
