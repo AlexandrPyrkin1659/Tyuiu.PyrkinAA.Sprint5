b77c08bbabeda5c153ad7b46310639a4bc1183cc
Александр Пыркин <Tyuiu.PyrkinAA@outlook.com>
2026-01-10 22:00:48 +0500

    1

--- Tyuiu.PyrkinAA.Sprint5.Task3.V24.Lib/DataService.cs
+++ Tyuiu.PyrkinAA.Sprint5.Task3.V24.Lib/DataService.cs
@@ -4,7 +4,7 @@ using tyuiu.cources.programming.interfaces.Sprint5;
 
 namespace Tyuiu.PyrkinAA.Sprint5.Task3.V24.Lib
 {
-    public class DataService : ISprint5Task3V21
+    public class DataService : ISprint5Task3V24
     {
         public string SaveToFileTextData(int x)
         {
