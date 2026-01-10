f40132f2e6b795ce5ddd0d929b2778f88cc359ed
Александр Пыркин <Tyuiu.PyrkinAA@outlook.com>
2026-01-10 18:32:07 +0500

    1

--- Tyuiu.PyrkinAA.Sprint5.Task2.V14.Lib/DataService.cs
+++ Tyuiu.PyrkinAA.Sprint5.Task2.V14.Lib/DataService.cs
@@ -1,30 +1,46 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using tyuiu.cources.programming.interfaces.Sprint5;
+
 namespace Tyuiu.PyrkinAA.Sprint5.Task2.V14.Lib
 {
     public class DataService : ISprint5Task2V14
     {
         public string SaveToFileTextData(int[,] matrix)
         {
-            string path = "result.csv"; 
+         
+            string fileName = "OutPutFileTask2.csv";
 
-            using (StreamWriter writer = new StreamWriter(path, false))
+          
+            if (File.Exists(fileName))
             {
-                int rows = matrix.GetLength(0);
-                int cols = matrix.GetLength(1);
+                File.Delete(fileName);
+            }
 
+          
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+         
+            using (StreamWriter writer = new StreamWriter(fileName, false))
+            {
                 for (int i = 0; i < rows; i++)
                 {
                     string[] values = new string[cols];
+
                     for (int j = 0; j < cols; j++)
                     {
+                      
                         values[j] = matrix[i, j] > 0 ? "1" : "0";
                     }
+
+                  
                     writer.WriteLine(string.Join(";", values));
                 }
             }
 
-            return Path.GetFullPath(path);
+           
+            return Path.GetFullPath(fileName);
         }
     }
 }
