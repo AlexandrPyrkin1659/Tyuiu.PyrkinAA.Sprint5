eebc8c3ae6d2c0774665f7fb4c714cfe0f3e9cd1
Александр Пыркин <Tyuiu.PyrkinAA@outlook.com>
2026-01-11 13:16:21 +0500

    1

--- Tyuiu.PyrkinAA.Sprint5.Task7.V17.Lib/DataService.cs
+++ Tyuiu.PyrkinAA.Sprint5.Task7.V17.Lib/DataService.cs
@@ -8,16 +8,16 @@ namespace Tyuiu.PyrkinAA.Sprint5.Task7.V17.Lib
     {
         public string LoadDataAndSave(string path)
         {
-            // Сохраняем в темповую папку (как в примере)
+            
             string outputPath = Path.Combine(Path.GetTempPath(), "OutPutDataFileTask7V17.txt");
 
-            // Читаем исходный файл
+           
             string text = File.ReadAllText(path, Encoding.UTF8);
 
-            // Удаляем все вхождения "нн"
+            
             string resultText = text.Replace("нн", "");
 
-            // Сохраняем результат
+          
             File.WriteAllText(outputPath, resultText, Encoding.UTF8);
 
             return outputPath;
--- Tyuiu.PyrkinAA.Sprint5.Task7.V17.Test/DataServiceTest.cs
+++ Tyuiu.PyrkinAA.Sprint5.Task7.V17.Test/DataServiceTest.cs
@@ -13,26 +13,25 @@ namespace Tyuiu.PyrkinAA.Sprint5.Task7.V17.Test
         {
             DataService ds = new DataService();
 
-            // Используем реальный путь из параметра теста
             string inputPath = @"C:\DataSprint5\InPutDataFileTask7V17.txt";
 
-            // Если файла нет - создаём временный тестовый
+          
             if (!File.Exists(inputPath))
             {
                 inputPath = Path.GetTempFileName();
                 File.WriteAllText(inputPath, "анна и джонн шли", Encoding.UTF8);
             }
 
-            // Выполняем метод
+           
             string outputPath = ds.LoadDataAndSave(inputPath);
 
-            // Проверяем, что файл создан
+            
             Assert.IsTrue(File.Exists(outputPath), "Выходной файл не создан");
 
-            // Читаем результат
+      
             string resultText = File.ReadAllText(outputPath, Encoding.UTF8);
 
-            // Проверяем, что "нн" удалены
+           
             Assert.IsFalse(resultText.Contains("нн"), "В результате остались 'нн'");
         }
 
