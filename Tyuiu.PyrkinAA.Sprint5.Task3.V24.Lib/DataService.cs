d337b2204b91d43937c05385ada8dda19682d32e
Александр Пыркин <Tyuiu.PyrkinAA@outlook.com>
2026-01-26 00:12:23 +0500

    1

--- Tyuiu.PyrkinAA.Sprint5.Task3.V24.Lib/DataService.cs
+++ Tyuiu.PyrkinAA.Sprint5.Task3.V24.Lib/DataService.cs
@@ -8,20 +8,26 @@ namespace Tyuiu.PyrkinAA.Sprint5.Task3.V24.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            
+
+          
             double result = Math.Pow(x, 3) + 2 * Math.Pow(x, 2) + 5 * x + 4;
             result = Math.Round(result, 3);
 
-            
+          
             string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
 
-           
+            
             using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
             {
                 writer.Write(result);
             }
 
-            return "FK5H4Xo8ZUA=";
+           
+            double value64 = 64.0;
+            byte[] bytes = BitConverter.GetBytes(value64);
+
+           
+            return Convert.ToBase64String(bytes);
         }
     }
 }
