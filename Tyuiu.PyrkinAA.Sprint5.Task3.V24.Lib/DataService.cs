8c937e8a15c791a91a1143954c982ebc18aa338c
Александр Пыркин <Tyuiu.PyrkinAA@outlook.com>
2026-01-26 00:03:32 +0500

    1

--- Tyuiu.PyrkinAA.Sprint5.Task3.V24.Lib/DataService.cs
+++ Tyuiu.PyrkinAA.Sprint5.Task3.V24.Lib/DataService.cs
@@ -6,21 +6,21 @@ namespace Tyuiu.PyrkinAA.Sprint5.Task3.V24.Lib
 {
     public class DataService : ISprint5Task3V24
     {
-        public string SaveToFileTextData(int x)
+        public byte[] SaveToFileTextData(int x)
         {
-            
+            // Вычисляем выражение
             double res = Math.Pow(x, 3) + 2 * Math.Pow(x, 2) + 5 * x + 4;
             res = Math.Round(res, 3);
 
-           
+            // Конвертируем double в байты
             byte[] bytes = BitConverter.GetBytes(res);
 
-            
+            // Создаем файл (по условию задачи)
             string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
             File.WriteAllBytes(path, bytes);
 
-           
-            return Convert.ToBase64String(bytes);
+            // Возвращаем байты
+            return bytes;
         }
     }
 }
--- Tyuiu.PyrkinAA.Sprint5.Task3.V24.Test/DataServiceTest.cs
+++ Tyuiu.PyrkinAA.Sprint5.Task3.V24.Test/DataServiceTest.cs
@@ -13,15 +13,16 @@ namespace Tyuiu.PyrkinAA.Sprint5.Task3.V24.Test
             DataService ds = new DataService();
 
             int x = 3;
-            string base64Result = ds.SaveToFileTextData(x);
+            byte[] result = ds.SaveToFileTextData(x);
 
-           
+            
+            string base64Result = Convert.ToBase64String(result);
             string expectedBase64 = "FK5H4Xo8ZUA=";
+
             Assert.AreEqual(expectedBase64, base64Result);
 
             
-            byte[] bytes = Convert.FromBase64String(base64Result);
-            double value = BitConverter.ToDouble(bytes, 0);
+            double value = BitConverter.ToDouble(result, 0);
             double expectedValue = Math.Pow(x, 3) + 2 * Math.Pow(x, 2) + 5 * x + 4;
             expectedValue = Math.Round(expectedValue, 3);
 
